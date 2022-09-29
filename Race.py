a5477d9df08d2df50eb5c75415cbf1d108a941a3
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2022-09-29 13:46:07 +0430

    asdfsdf

--- Race.py
+++ Race.py
@@ -14,7 +14,7 @@ class Race:
     def declare_winner(cars):
         speeds = [{'car_name': car.name, 'car_speed': car.speed} for car in cars]
         speeds.sort(key=lambda self: self["car_speed"])
-        winner = speeds[len(speeds) - 1] 
+        winner = speeds[-1] 
         print(f"AND THE WINNER IS {winner['car_name']}!!!")
 
 cars_names = ["4Runner", "Corolla", "F2", "Hilux", "Mazda", "BMW"]
